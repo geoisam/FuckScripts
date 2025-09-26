7d86c115db06b1e3ee44b926bc35660f3ff0a418
潘钜森 <2858727468@qq.com>
2025-09-27 00:01:44 +0800

    Update zepplife.py

--- zepplife.py
+++ zepplife.py
@@ -113,4 +113,4 @@ if __name__ == "__main__":
         password = su.get("pwd", "")
         steps = str(randint(minSteps, maxSteps))
         main()
-        time.sleep(randint(10, 30))
+        time.sleep(randint(10, 20))
